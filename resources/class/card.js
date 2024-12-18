9b5a76ef2a95667cec57223bf3389fb6dad03e7c
fre11896 <Austen.Freeman@Fluor.com>
2024-12-18 11:48:29 +0000

    181224-1

---
+++ resources/class/card.js
@@ -0,0 +1,14 @@
+class Card {
+    constructor(name, number, pips) {
+      this.name = name;
+      this.number = number;
+      this.pips = pips;
+    }
+  
+    //myMethod() {
+      // Method logic here
+      //console.log("MyClass method called!"); 
+    //}
+  }
+
+  export { Card };
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,26 +1,34 @@
+//import { Card } from "../class/card";
 
+class Card {
+    constructor(name, number, pips) {
+      this.name = name;
+      this.number = number;
+      this.pips = pips;
+    }
+  }
 
 const actioncards = [
-    ["Construction2"],
-    ["Construction3"],
-    ["Construction4"],
-    ["Construction5"],
-    ["Construction6"],
-    ["Aggression2"],
-    ["Aggression3"],
-    ["Aggression4"],
-    ["Aggression5"],
-    ["Aggression6"],
-    ["Administration2"],
-    ["Administration3"],
-    ["Administration4"],
-    ["Administration5"],
-    ["Administration6"],
-    ["Mobilisation2"],
-    ["Mobilisation3"],
-    ["Mobilisation4"],
-    ["Mobilisation5"],
-    ["Mobilisation6"]
+   new Card("Construction", "2", "2"),
+   new Card("Construction", "3", "2"),
+   new Card("Construction", "4", "2"),
+   new Card("Construction", "5", "2"),
+   new Card("Construction", "6", "2"),
+   new Card("Aggression", "2", "2"),
+   new Card("Aggression", "3", "2"),
+   new Card("Aggression", "4", "2"),
+   new Card("Aggression", "5", "2"),
+   new Card("Aggression", "6", "2"),
+   new Card("Administration", "2", "2"),
+   new Card("Administration", "3", "2"),
+   new Card("Administration", "4", "2"),
+   new Card("Administration", "5", "2"),
+   new Card("Administration", "6", "2"),
+   new Card("Mobilisation", "2", "2"),
+   new Card("Mobilisation", "3", "2"),
+   new Card("Mobilisation", "4", "2"),
+   new Card("Mobilisation", "5", "2"),
+   new Card("Mobilisation", "6", "2"),
 ];
 
 const currentactioncards = actioncards.slice();
@@ -32,6 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //currentactioncards = actioncards.slice();
 });
 
+
 function testbtn(){
     let playerNumber = 1;
     do 
@@ -39,6 +48,8 @@ function testbtn(){
         let num = Math.floor(Math.random() * currentactioncards.length);
         //alert(num);
         //alert(currentactioncards[num]);
+
+        //const a = new Card("Construction", "2", "2");
 
         if (playerNumber == 1){
             player1hand.push(currentactioncards[num]);
@@ -61,5 +72,17 @@ function testbtn(){
     //while (currentactioncards.length != 0)
 
     alert(player1hand);
-    alert(player2hand);
+    //alert(player2hand);
+
+    player1hand.forEach(card => {
+        //let btn = document.createElement("input");
+        //btn.type = "checkbox"
+        //btn.title = card;
+        let btn = document.createElement("button");
+        btn.innerHTML = card.name + card.number;
+        btn.onclick = function () {
+            btn.disabled = true;
+        };
+        document.getElementById("player1hand").append(btn);
+    })
 }
